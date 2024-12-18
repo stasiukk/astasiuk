82f6bee8bd2142e6b2f841504c47e89d137629c8
stasiukk <andriistasiuk26@gmail.com>
2024-12-18 00:02:12 -0500

    w

--- script.js
+++ script.js
@@ -17,5 +17,38 @@ function filterProjects(category) {
     });
 }
 
+const progressCircles = document.querySelectorAll('.progress-circle');
+
+progressCircles.forEach((circle) => {
+    const progress = circle.getAttribute('data-progress');
+    circle.style.setProperty('--progress', progress / 100);
+});
+
+
 // AOS Animation Initialization
 AOS.init();
+
+const skills = [
+    { id: "number1", percentage: 90 },
+    { id: "number2", percentage: 80 },
+    { id: "number4", percentage: 89 },
+    { id: "number5", percentage: 70 },
+    { id: "number6", percentage: 89 },
+    { id: "number7", percentage: 76 },
+    { id: "number3", percentage: 88 },
+];
+
+skills.forEach(skill => {
+    let counter = 0;
+    const target = skill.percentage;
+    const numberElement = document.getElementById(skill.id);
+    const interval = setInterval(() => {
+        if (counter <= target) {
+            numberElement.innerText = `${counter}%`;
+            counter++;
+        } else {
+            clearInterval(interval);
+        }
+    }, 30);
+});
+
